892c11d11e0f378cb8b1206b327ee9a098ad3922
Camila Morales <87491325+Camila-Morales@users.noreply.github.com>
2024-11-29 11:52:35 -0500

    Add files via upload

---
+++ DAL/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DAL
+{
+    public interface IRepository : IDisposable
+    {
+        TEntity Create<TEntity>(TEntity toCreate) where TEntity : class;
+        bool Delete<TEntity>(TEntity toDelete) where TEntity : class;
+        bool Update<TEntity>(TEntity toUpdate) where TEntity : class;
+
+        // Nos devuelve un único registro
+        TEntity Retrieve<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+
+        // Filtra entidades según un criterio
+        List<TEntity> Filter<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+
+        // Método que lista todos los registros de una entidad
+        List<TEntity> ListAll<TEntity>() where TEntity : class;
+    }
+}
