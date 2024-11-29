6c03b423fa2e8baea3aef931d57c464505402699
Camila Morales <87491325+Camila-Morales@users.noreply.github.com>
2024-11-29 11:51:39 -0500

    Add files via upload

---
+++ BLL/CategoryLogic.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+
+namespace BLL
+{
+    public class CategoryLogic
+    {
+        // MÉTODO PARA CREAR UNA CATEGORÍA
+        public Categories Create(Categories newCategory)
+        {
+            Categories result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                // Busca si el nombre de la categoría existe
+                Categories res =
+                    r.Retrieve<Categories>(
+                        c => c.CategoryName == newCategory.CategoryName);
+
+                if (res == null)
+                {
+                    // No existe, podemos crearla
+                    result = r.Create(newCategory);
+                }
+                else
+                {
+                    // Podríamos lanzar una excepción para notificar que la categoría ya existe
+                }
+            }
+            return result;
+        }
+
+        // BÚSQUEDA POR ID
+        public Categories RetrieveByID(int id)
+        {
+            Categories result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                result = r.Retrieve<Categories>(c => c.CategoryID == id);
+            }
+            return result;
+        }
+
+        // MÉTODO PARA ACTUALIZAR
+        public bool Update(Categories categoryToUpdate)
+        {
+            bool res = false;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                // Validar que el nombre de la categoría NO exista
+                Categories temp =
+                    r.Retrieve<Categories>(
+                        c => c.CategoryName == categoryToUpdate.CategoryName
+                        && c.CategoryID != categoryToUpdate.CategoryID);
+
+                if (temp == null)
+                {
+                    // No existe
+                    res = r.Update(categoryToUpdate);
+                }
+                else
+                {
+                    // Podríamos implementar alguna lógica para indicar que no se pudo modificar
+                }
+            }
+            return res;
+        }
+
+        // MÉTODO PARA ELIMINAR
+        public bool Delete(int id)
+        {
+            bool res = false;
+
+            // Buscar la categoría por ID
+            var category = RetrieveByID(id);
+
+            if (category != null)
+            {
+                // Validar si tiene dependencias antes de eliminarla
+                using (var r = RepositoryFactory.CreateRepository())
+                {
+                    res = r.Delete(category);
+                }
+            }
+            else
+            {
+                // Podríamos implementar lógica para indicar que la categoría no existe
+            }
+
+            return res;
+        }
+
+        // LISTAR TODAS LAS CATEGORÍAS POR NOMBRE
+        public List<Categories> FilterByName(string categoryName)
+        {
+            List<Categories> result = null;
+
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                result = r.Filter<Categories>(
+                    c => c.CategoryName.Contains(categoryName));
+            }
+            return result;
+        }
+
+        public List<Categories> ListAll()
+        {
+            List<Categories> result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                result = r.ListAll<Categories>();  // Suponiendo que el repositorio tenga este método
+            }
+            return result;
+        }
+
+    }
+}
---
+++ BLL/ProductLogic.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+
+namespace BLL
+{
+    public class ProductLogic
+    {
+        // METODO PARA CREAR EL PRODUCTO
+        public Products Create(Products newProduct)
+        {
+            Products Result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                // Busca si el nombre del producto existe
+                Products res = r.Retrieve<Products>(
+                    p => p.ProductName == newProduct.ProductName);
+
+                if (res == null)
+                {
+                    // No existe, podemos crearlo
+                    Result = r.Create(newProduct);
+                }
+                else
+                {
+                    // Podriamos lanzar una excepcion para notificar que el producto ya existe
+                    // Podriamos incluso crear una capa de Excepciones personalizadas y consumirla desde otras capas
+                }
+            }
+            return Result;
+        }
+
+        // BUSQUEDA POR ID
+        public Products RetrieveByID(int id)
+        {
+            Products Result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                Result = r.Retrieve<Products>(p => p.ProductID == id);
+            }
+            return Result;
+        }
+
+        // METODO DE ACTUALIZAR
+        public bool Update(Products productToUpdate)
+        {
+            bool res = false;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                // Validar que el nombre del producto NO exista
+                Products temp = r.Retrieve<Products>(
+                    p => p.ProductName == productToUpdate.ProductName
+                    && productToUpdate.ProductID != productToUpdate.ProductID);
+
+                if (temp == null)
+                {
+                    // No existe
+                    res = r.Update(productToUpdate);
+                }
+                else
+                {
+                    // Podemos implementar alguna lógica para indicar que no se pudo modificar
+                }
+            }
+            return res;
+        }
+
+        // METODO PARA ELIMINAR
+        public bool Delete(int ID)
+        {
+            bool res = false;
+
+            // Buscar el producto para ver si tiene existencias
+            var product = RetrieveByID(ID);
+
+            if (product != null)
+            {
+                // Eliminar el producto
+                if (product.UnitsInStock == 0)
+                {
+                    using (var r = RepositoryFactory.CreateRepository())
+                    {
+                        res = r.Delete(product);
+                    }
+                }
+                else
+                {
+                    // Podemos implementar alguna lógica adicional para indicar que no se pudo eliminar el producto
+                }
+            }
+            else
+            {
+                // Podemos implementar alguna lógica para indicar que el producto no existe
+            }
+
+            return res;
+        }
+
+        // LISTAR TODOS LOS PRODUCTOS
+        public List<Products> ListAll()
+        {
+            List<Products> result = null;
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                result = r.ListAll<Products>();  // Suponiendo que el repositorio tenga este método
+            }
+            return result;
+        }
+
+        // LISTAR TODOS LOS PRODUCTOS POR CATEGORIA
+        public List<Products> FilterByCategoryID(int categoryID)
+        {
+            List<Products> result = null;
+
+            using (var r = RepositoryFactory.CreateRepository())
+            {
+                result = r.Filter<Products>(
+                    p => p.CategoryID == categoryID);
+            }
+            return result;
+        }
+    }
+}
