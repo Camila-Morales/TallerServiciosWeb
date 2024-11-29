df8faf26898fbca72e5ec313fcf794f5765bbef1
Camila Morales <87491325+Camila-Morales@users.noreply.github.com>
2024-11-29 11:52:58 -0500

    Add files via upload

---
+++ SLC/ICategoryService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace SLC
+{
+    public interface ICategoryService
+    {
+        // Crear una nueva categoría
+        Categories Create(Categories categories);
+
+        // Eliminar una categoría por ID
+        bool Delete(int id);
+
+        // Buscar una categoría por ID
+        Categories RetrieveByID(int id);
+
+        // Listar todas las categorías
+        List<Categories> ListAll();
+
+        // Actualizar una categoría existente
+        bool Update(Categories categories);
+    }
+}
---
+++ SLC/IProductService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace SLC
+{
+    public interface IProductService
+    {
+        // Crear un nuevo producto
+        Products Create(Products product);
+
+        // Eliminar un producto por ID
+        bool Delete(int id);
+
+        // Buscar un producto por ID
+        Products RetrieveByID(int id);
+
+        // Listar todos los productos
+        List<Products> ListAll();
+
+        // Actualizar un producto existente
+        bool Update(Products product);
+    }
+}
