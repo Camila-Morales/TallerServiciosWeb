2712cb4cc968fba21045176e73e5ac0389bb17b9
Camila Morales <87491325+Camila-Morales@users.noreply.github.com>
2024-11-29 11:53:31 -0500

    Add files via upload

---
+++ Service1/Controllers/CategoriesController.cs
@@ -0,0 +1,59 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Entities;
+using SLC;
+
+namespace Service.Controllers
+{
+    public class CategoryController : ApiController, ICategoryService
+    {
+        // CREAR
+        [HttpPost]
+        public Categories Create(Categories category)
+        {
+            var categoryLogic = new CategoryLogic();
+            var createdCategory = categoryLogic.Create(category);
+            return createdCategory;
+        }
+
+        // ELIMINAR
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            var categoryLogic = new CategoryLogic();
+            var result = categoryLogic.Delete(id);
+            return result;
+        }
+
+        // BUSCAR POR ID
+        [HttpGet]
+        public Categories RetrieveByID(int id)
+        {
+            var categoryLogic = new CategoryLogic();
+            var category = categoryLogic.RetrieveByID(id);
+            return category;
+        }
+
+        // LISTAR TODAS LAS CATEGORÍAS
+        [HttpGet]
+        public List<Categories> ListAll()
+        {
+            var categoryLogic = new CategoryLogic();
+            var categories = categoryLogic.FilterByName(""); // Sin filtro para listar todas
+            return categories;
+        }
+
+        // ACTUALIZAR
+        [HttpPut]
+        public bool Update(Categories category)
+        {
+            var categoryLogic = new CategoryLogic();
+            var result = categoryLogic.Update(category);
+            return result;
+        }
+    }
+}
---
+++ Service1/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Entities;
+using SLC;
+
+namespace Service.Controllers
+{
+    public class ProductController : ApiController, IProductService
+    {
+        // CREAR
+        [HttpPost]
+        public Products Create(Products products)
+        {
+            var productLogic = new ProductLogic();
+            var product = productLogic.Create(products);
+            return product;
+        }
+
+        // ELIMINAR
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            var productLogic = new ProductLogic();
+            var product = productLogic.Delete(id);
+            return product;
+        }
+
+        // BUSCAR POR ID
+        [HttpGet]
+        public Products RetrieveByID(int id)
+        {
+            var productLogic = new ProductLogic();
+            var product = productLogic.RetrieveByID(id);
+            return product;
+        }
+
+        // LISTAR TODOS LOS PRODUCTOS
+        [HttpGet]
+        public List<Products> ListAll()
+        {
+            var productLogic = new ProductLogic();
+            var products = productLogic.ListAll();
+            return products;
+        }
+
+        // ACTUALIZAR
+        [HttpPut]
+        public bool Update(Products products)
+        {
+            var productLogic = new ProductLogic();
+            var updated = productLogic.Update(products);
+            return updated;
+        }
+    }
+}
